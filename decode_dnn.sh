7567ef42ef016fc8e457ca12924e2fa883bc0ad7
Shawn Tan <shawn@wtf.sg>
2015-02-11 11:57:16 +0800

    Modifications to decode script for new featureset.

--- decode_dnn.sh
+++ decode_dnn.sh
@@ -76,11 +76,15 @@ $cmd $dir/log/class_count.log \
 
 ## Set up the features
 echo "$0: feature: splice(${splice_opts}) norm_vars(${norm_vars})"
+feats="copy-feats scp:$sdata/JOB/feats.scp ark:- \
+	| add-deltas --delta-order=$(cat $dir/../delta_order) ark:- ark:- \
+	| splice-feats $splice_opts ark:- ark:- \
+	| nnet-forward $dir/../feature_transform ark:- ark,t:-"
 
-feats="apply-cmvn --norm-vars=$norm_vars --utt2spk=ark:$sdata/JOB/utt2spk scp:$sdata/JOB/cmvn.scp scp:$sdata/JOB/feats.scp ark:- | splice-feats $splice_opts ark:- ark,t:- |"
+#feats="apply-cmvn --norm-vars=$norm_vars --utt2spk=ark:$sdata/JOB/utt2spk scp:$sdata/JOB/cmvn.scp scp:$sdata/JOB/feats.scp ark:- | splice-feats $splice_opts ark:- ark,t:- |"
 ##
 #finalfeats="$feats nnet-forward --class-frame-counts=$dir/class.counts --apply-log=true --no-softmax=false $srcdir/dnn.nnet ark:- ark:- |"
-finalfeats="ark,s,cs:$feats python2 theano-kaldi/nnet_forward.py $(cat $dir/../structure) $srcdir/dnn.pkl $dir/class.counts |"
+finalfeats="ark,s,cs:$feats | python2 theano-kaldi/nnet_forward.py $(cat $dir/../structure) $srcdir/dnn.pkl $dir/class.counts |"
 $cmd JOB=1:$nj $dir/log/decode.JOB.log \
   latgen-faster-mapped --max-active=$max_active --beam=$beam --lattice-beam=$latbeam --acoustic-scale=$acwt --allow-partial=true --word-symbol-table=$graphdir/words.txt $alidir/final.mdl $graphdir/HCLG.fst "$finalfeats" "ark:|gzip -c > $dir/lat.JOB.gz"
 
--- run.sh
+++ run.sh
@@ -15,43 +15,43 @@ mkdir -p $dir/data
 echo \
 "--use-energy=true
 --num-mel-bins=40" > conf/fbank.conf
-#for set in train dev test
-#do
-#	cp -r data/$set $dir/data/$set
-#	rm -rf $dir/data/$set/{cmvn,feats}.scp $dir/data/$set/split*
-#	steps/make_fbank.sh --fbank-config conf/fbank.conf --cmd "run.pl" --nj 10 $dir/data/$set $dir/_log $dir/_fbank || exit 1;
-##	steps/compute_cmvn_stats.sh                $dir/data/$set $dir/_log $dir/_fbank || exit 1;
-#done
+for set in train dev test
+do
+	cp -r data/$set $dir/data/$set
+	rm -rf $dir/data/$set/{cmvn,feats}.scp $dir/data/$set/split*
+	steps/make_fbank.sh --fbank-config conf/fbank.conf --cmd "run.pl" --nj 10 $dir/data/$set $dir/_log $dir/_fbank || exit 1;
+#	steps/compute_cmvn_stats.sh                $dir/data/$set $dir/_log $dir/_fbank || exit 1;
+done
 
-#copy-feats scp:$dir/data/train/feats.scp ark:- \
-#	| add-deltas --delta-order=$(cat $dir/delta_order) ark:- ark:- \
-#	| splice-feats $splice_opts ark:- ark:- \
-#	| compute-cmvn-stats ark:- - \
-#	| cmvn-to-nnet --binary=false - $dir/feature_transform 
-#
-#mkdir -p $dir/pkl
-#time copy-feats scp:$dir/data/train/feats.scp ark:- \
-#	| add-deltas --delta-order=$(cat $dir/delta_order) ark:- ark:- \
-#	| splice-feats $splice_opts ark:- ark:- \
-#	| nnet-forward $dir/feature_transform ark:- ark,t:-  \
-#	| python2 $TK_DIR/pickle_ark_stream.py $dir/pkl/train.pklgz $dir/input_dim
+copy-feats scp:$dir/data/train/feats.scp ark:- \
+	| add-deltas --delta-order=$(cat $dir/delta_order) ark:- ark:- \
+	| splice-feats $splice_opts ark:- ark:- \
+	| compute-cmvn-stats ark:- - \
+	| cmvn-to-nnet --binary=false - $dir/feature_transform 
 
-#python2 $TK_DIR/picklise_lbl.py $ali_dir $dir/pkl/train_lbl.pklgz
+mkdir -p $dir/pkl
+time copy-feats scp:$dir/data/train/feats.scp ark:- \
+	| add-deltas --delta-order=$(cat $dir/delta_order) ark:- ark:- \
+	| splice-feats $splice_opts ark:- ark:- \
+	| nnet-forward $dir/feature_transform ark:- ark,t:-  \
+	| python2 $TK_DIR/pickle_ark_stream.py $dir/pkl/train.pklgz $dir/input_dim
+
+python2 $TK_DIR/picklise_lbl.py $ali_dir $dir/pkl/train_lbl.pklgz
 
 
-num_pdfs=`gmm-info $gmmdir/final.mdl | grep pdfs | awk '{print $NF}'`
-structure="$(cat $dir/input_dim):1024:1024:1024:1024:1024:1024:$num_pdfs"
-echo $structure > $dir/structure
-#structure=$(cat $dir/structure)
+um_pdfs=`gmm-info $gmmdir/final.mdl | grep pdfs | awk '{print $NF}'`
+tructure="$(cat $dir/input_dim):1024:1024:1024:1024:1024:1024:$num_pdfs"
+cho $structure > $dir/structure
+structure=$(cat $dir/structure)
 
-#python $TK_DIR/split_dataset.py \
-#	$dir/pkl/train.pklgz \
-#	$dir/pkl/train_lbl.pklgz  \
-#	0.05 \
-#	$dir/pkl/trn.train.pklgz \
-#	$dir/pkl/trn.train_lbl.pklgz \
-#	$dir/pkl/val.train.pklgz \
-#	$dir/pkl/val.train_lbl.pklgz 
+python $TK_DIR/split_dataset.py \
+	$dir/pkl/train.pklgz \
+	$dir/pkl/train_lbl.pklgz  \
+	0.05 \
+	$dir/pkl/trn.train.pklgz \
+	$dir/pkl/trn.train_lbl.pklgz \
+	$dir/pkl/val.train.pklgz \
+	$dir/pkl/val.train_lbl.pklgz 
 
 python $TK_DIR/pretrain_sda.py\
 	--frames-file $dir/pkl/trn.train.pklgz \
